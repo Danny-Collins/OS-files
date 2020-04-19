63bc605ada55e1ca91a529a7cc78ce8b82170bce
Danny-Collins <54964062+Danny-Collins@users.noreply.github.com>
2020-04-19 17:05:31 -0400

    Update hello.c

--- hello/hello.c
+++ hello/hello.c
@@ -3,13 +3,13 @@
 
 //  Define the module metadata.
 #define MODULE_NAME "hello"
-MODULE_AUTHOR("YOUR NAME HERE");
+MODULE_AUTHOR("Daniel Collins");
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("A simple kernel module to greet the installer");
 MODULE_VERSION("0.1");
 
 //  Define the name parameter.
-static char *name = "YOUR STEVENS USERNAME HERE";
+static char *name = "dcollin4";
 module_param(name, charp, S_IRUGO);
 MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");
 
@@ -17,13 +17,18 @@ static int __init hello_init(void)
 {
     pr_info("%s: module loaded at 0x%p\n", MODULE_NAME, hello_init);
     // TODO say hello to the named user
+    printk("Hello %s!", name);
     return 0;
 }
 
 static void __exit hello_exit(void)
 {
     // TODO say goodbye to the named user
+    printk("Goodbye %s!", name);
     pr_info("%s: module unloaded from 0x%p\n", MODULE_NAME, hello_exit);
 }
 
 // TODO register the operations to be executed when the KM is loaded and unloaded
+
+module_init(hello_init);
+module_exit(exit_init);
